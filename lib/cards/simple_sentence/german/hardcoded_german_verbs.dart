e6f62d2a843b1edc7ef0e3c5e1d5f3b6f21e8253
Aleksandr Mikhailov <am5800@yandex.ru>
2020-11-12 14:18:18 +0100

    Some more hardcoded exercises

--- lib/cards/simple_sentence/german/hardcoded_german_verbs.dart
+++ lib/cards/simple_sentence/german/hardcoded_german_verbs.dart
@@ -6,6 +6,8 @@ import 'package:harmonie/cards/card.dart';
 import 'package:harmonie/cards/exercises/rich_text_exercise.dart';
 import 'package:harmonie/common/card_study_result.dart';
 
+enum GermanCase { Nominativ, Accusativ, Dativ, Genitiv }
+
 class VerbPrepositionSentence {
   final String de;
   final String en;
@@ -15,36 +17,52 @@ class VerbPrepositionSentence {
   VerbPrepositionSentence(
       this.de, this.en, this.verbIndices, this.prepositionIndices);
 
-  static VerbPrepositionSentence sichSpezialisieren1 = VerbPrepositionSentence(
-      "Der Zoologe spezialisiert sich auf die Meeresbiologie",
-      "The zoologist specializes in marine biology",
-      [2, 3],
-      [4]);
+  static VerbPrepositionSentence _sichSpezialisieren1Auf =
+      VerbPrepositionSentence(
+          "Der Zoologe spezialisiert sich auf die Meeresbiologie",
+          "The zoologist specializes in marine biology",
+          [2, 3],
+          [4]);
 
-  static VerbPrepositionSentence sichSpezialisieren2 = VerbPrepositionSentence(
+  static VerbPrepositionSentence _sichSpezialisieren2Auf = VerbPrepositionSentence(
       "Sie spezialisieren sich auf die Beschreibung Ihrer Produkte und Kategorienamen",
       "They are specialized in the description of your products and category names",
       [1, 2],
       [3]);
 
-  static VerbPrepositionSentence sichSpezialisieren3 = VerbPrepositionSentence(
+  static VerbPrepositionSentence _sichSpezialisieren3Auf = VerbPrepositionSentence(
       "Dadurch können Sie Ihr persönliches Profil entwickeln oder sich auf Schwerpunkte spezialisieren",
       "You can develop your personal profile, to specialize in your favour area",
       [8, 11],
       [9]);
 
-  static List<VerbPrepositionSentence> sichSpezialisieren = [
-    sichSpezialisieren1,
-    sichSpezialisieren2,
-    sichSpezialisieren3
+  static List<VerbPrepositionSentence> sichSpezialisierenAuf = [
+    _sichSpezialisieren1Auf,
+    _sichSpezialisieren2Auf,
+    _sichSpezialisieren3Auf
+  ];
+
+  static VerbPrepositionSentence _arbeitenAn = VerbPrepositionSentence(
+      "Unsere Entwickler arbeiten an einer neue Version dieses machtigen Form-Baumeisters",
+      "Our developers work on a new version of this powerful form builder",
+      [2],
+      [3]);
+
+  static List<VerbPrepositionSentence> arbeitenAn = [
+    _arbeitenAn,
   ];
 }
 
-class SichSpezialisieren implements Card {
+class VerbMeaningCard implements Card {
+  final String _id;
+  final String _en;
+  final List<VerbPrepositionSentence> _sentences;
+
+  VerbMeaningCard(this._id, this._en, this._sentences);
+
   @override
   Exercise getExercise(Random random) {
-    final sentences = VerbPrepositionSentence.sichSpezialisieren;
-    final sentence = sentences[random.nextInt(sentences.length)];
+    final sentence = _sentences[random.nextInt(_sentences.length)];
 
     final splitted = sentence.de.split(" ").toList();
 
@@ -53,22 +71,33 @@ class SichSpezialisieren implements Card {
     }
 
     final String front = splitted.join(" ");
-    final String back =
-        "<accent>to specialize in something</accent>\n${sentence.en}";
+    final String back = "<accent>$_en</accent>\n${sentence.en}";
 
     return RichTextExercise(front, front, back, CardStudyResult.values);
   }
 
   @override
-  CardId get id => CardId("sich_spezialisieren", HardcodedGermanVerbFactory.Id);
+  CardId get id => CardId(_id, HardcodedGermanVerbFactory.Id);
+
+  static VerbMeaningCard sichSpezialisierenAuf = VerbMeaningCard(
+      "sich_spezialisieren",
+      "To specialize in something",
+      VerbPrepositionSentence.sichSpezialisierenAuf);
+
+  static VerbMeaningCard arbeitenAn = VerbMeaningCard(
+      "arbeiten", "To work on", VerbPrepositionSentence.arbeitenAn);
 }
 
-class SichSpezialisierenFuer implements Card {
-  // TODO: dependency
+class VerbPrepositionCard implements Card {
+  final String _id;
+  final List<VerbMeaningCard> dependsOn;
+  final List<VerbPrepositionSentence> _sentences;
+  final GermanCase _case;
+
+  VerbPrepositionCard(this._id, this.dependsOn, this._sentences, this._case);
 
   Exercise getExercise(Random random) {
-    final sentences = VerbPrepositionSentence.sichSpezialisieren;
-    final sentence = sentences[random.nextInt(sentences.length)];
+    final sentence = _sentences[random.nextInt(_sentences.length)];
 
     final splitted1 = sentence.de.split(" ").toList();
 
@@ -84,15 +113,26 @@ class SichSpezialisierenFuer implements Card {
 
     final String front = splitted2.join(" ");
     final String frontUncovered = splitted1.join(" ");
-    final String back =
-        "<accent>to specialize in something</accent>\nAccusative\n${sentence.en}";
+    final String back = "${sentence.en}\n$_case";
 
-    return RichTextExercise(front, frontUncovered, back, CardStudyResult.values);
+    return RichTextExercise(
+        front, frontUncovered, back, CardStudyResult.values);
   }
 
   @override
-  CardId get id =>
-      CardId("sich_spezialisieren_fuer", HardcodedGermanVerbFactory.Id);
+  CardId get id => CardId(_id, HardcodedGermanVerbFactory.Id);
+
+  static VerbPrepositionCard sichSpezialisierenAuf = VerbPrepositionCard(
+      "sich_spezialisieren_auf",
+      [VerbMeaningCard.sichSpezialisierenAuf],
+      VerbPrepositionSentence.sichSpezialisierenAuf,
+      GermanCase.Accusativ);
+
+  static VerbPrepositionCard arbeitenAn = VerbPrepositionCard(
+      "arbeiten_an",
+      [VerbMeaningCard.arbeitenAn],
+      VerbPrepositionSentence.arbeitenAn,
+      GermanCase.Dativ);
 }
 
 class HardcodedGermanVerbFactory implements CardFactory {
@@ -100,8 +140,9 @@ class HardcodedGermanVerbFactory implements CardFactory {
   final Map<CardId, Card> _cards = LinkedHashMap();
 
   HardcodedGermanVerbFactory() {
-    _addCard(SichSpezialisieren());
-    _addCard(SichSpezialisierenFuer());
+    _addCard(VerbMeaningCard.sichSpezialisierenAuf);
+    _addCard(VerbMeaningCard.arbeitenAn);
+    _addCard(VerbPrepositionCard.sichSpezialisierenAuf);
   }
 
   void _addCard(Card card) {
